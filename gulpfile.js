ee83c1e3da51cc9cfbce21afa624d9c19c90e30f
Dave Hoeks <dave.hoeks@gmail.com>
2017-12-10 13:33:07 +0100

    Added concatination of JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 const gulp = require("gulp"),
-	  gutil = require("gulp-util");
+	  gutil = require("gulp-util"),
+	  concat = require("gulp-concat");
 
+var jsSources = [
+	"components/scripts/hello.js",
+	"components/scripts/world.js"
+];
 
+gulp.task("js", function() {
+	gulp.src(jsSources)
+		.pipe(concat("main.js"))
+		.pipe(gulp.dest("builds/development/js"))
+});
