b3ab08313759227c6cd08d2cc97ff8a988e513a2
Dave Hoeks <dave.hoeks@gmail.com>
2017-12-10 15:40:27 +0100

    Added sass compiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-const gulp = require("gulp"),
-	  gutil = require("gulp-util"),
-	  browserify = require("gulp-browserify"),
-	  concat = require("gulp-concat");
+var gulp = require("gulp"),
+	gutil = require("gulp-util"),
+	browserify = require("gulp-browserify"),
+	concat = require("gulp-concat"),
+	sass = require("gulp-sass");
 
 var jsSources = [
 	"components/scripts/hello.js",
 	"components/scripts/world.js"
 ];
 
+var sassSources = ["components/sass/style.scss"];
+
 gulp.task("js", function() {
 	gulp.src(jsSources)
 		.pipe(concat("main.js"))
 		.pipe(browserify())
 		.pipe(gulp.dest("builds/development/js"))
+});
+
+gulp.task("sass", function() {
+	return gulp.src(sassSources)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('builds/development/css'));
 });
